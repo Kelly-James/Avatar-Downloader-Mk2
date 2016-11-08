708d2f59354377669106ccb9e0d761e0aefcebfd
Kelly James <kellyjamesclancy@gmail.com>
2016-11-08 03:18:53 +0000

    Makes requests to specified URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
 const request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+
+  request('https://api.github.com/repos/', (err, response, body) => {
+    if(err) throw err;
+    console.log('Response Status Code: ', response.statusCode);
+  })
+
+}
+
+// function printAvatarURLs(repoList) {
+//   repoList.forEach((repoObject) => {
+//     console.log(repoObject[avatar_url]);
+//   });
+// }
+
+getRepoContributors('jquery', 'jquery', (err, result) => {
+  console.log('Errors: ', err);
+  console.log('Result: ', result);
+});
