b58ceb40de3eab4ef82c58fcc1e52f44ce66927b
Kelly James <kellyjamesclancy@gmail.com>
2016-11-08 04:41:45 +0000

    Added function to pipe response data to disc; successfully downloaded an avatar

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 const github_user = 'Kelly-James';
 const github_token = 'a9bab058f320076c2dd8a5d7340b32d28b825985';
-
+const fs = require('fs');
 // '.defaults' is a convienience method of 'require' to add options to the request header
 const request = require('request').defaults({
   headers: {
@@ -36,6 +36,22 @@ function printAvatarURLs(repoList) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+        .on('error', function(err) {
+          throw err;
+        })
+        .on('response', function(response) {
+          console.log('Response Status Message: ', response.statusMessage);
+          console.log('Response Content-Type: ', response.headers['content-type']);
+          console.log('Downloading image ... ');
+        })
+        .pipe(fs.createWriteStream(filePath))
+        .on('finish', () => {
+          console.log('Download Complete.');
+        })
+};
+
 getRepoContributors('jquery', 'jquery', (err, result) => {
   if(err)
   {
@@ -44,3 +60,5 @@ getRepoContributors('jquery', 'jquery', (err, result) => {
   }
   printAvatarURLs(result);
 });
+
+downloadImageByURL('https://avatars.githubusercontent.com/u/1615?v=3', 'avatars/avatar.jpg');
