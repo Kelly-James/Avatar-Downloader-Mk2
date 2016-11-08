7b73f2262b78bec5f5e3fb9e666991706faea01c
Kelly James <kellyjamesclancy@gmail.com>
2016-11-08 04:19:38 +0000

    Added convienience method to request method; added function to print avatar_urls to the console; successfully made request to github API

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,46 @@
-const request = require('request');
+const github_user = 'Kelly-James';
+const github_token = 'a9bab058f320076c2dd8a5d7340b32d28b825985';
+
+// '.defaults' is a convienience method of 'require' to add options to the request header
+const request = require('request').defaults({
+  headers: {
+    'User-Agent': 'MyImageDownloader/1.0'
+  },
+  json: true,
+  auth: {
+    user: github_user,
+    pass: github_token
+  }
+});
 
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, callback) {
+  var requestURL = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
 
-  request('https://api.github.com/repos/', (err, response, body) => {
-    if(err) throw err;
-    console.log('Response Status Code: ', response.statusCode);
+  console.log(requestURL);
+
+  request.get(requestURL, (err, response, body) => {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, response.body);
   })
 
 }
 
-// function printAvatarURLs(repoList) {
-//   repoList.forEach((repoObject) => {
-//     console.log(repoObject[avatar_url]);
-//   });
-// }
+// function which loops through the response body and prints the avatar urls to the console
+function printAvatarURLs(repoList) {
+  repoList.forEach((repoObject) => {
+    console.log(repoObject.avatar_url);
+  });
+}
 
 getRepoContributors('jquery', 'jquery', (err, result) => {
-  console.log('Errors: ', err);
-  console.log('Result: ', result);
+  if(err)
+  {
+    console.error('Errors: ', err);
+    return;
+  }
+  printAvatarURLs(result);
 });
